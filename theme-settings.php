e7c3cab6e5c35aff735385efa4c777b1f6254b2c
Brian Young <brianyoung@stanford.edu>
2014-02-25 12:15:43 -0800

    Added color palette and typography theme options

--- theme-settings.php
+++ theme-settings.php
@@ -1,56 +1,46 @@
 <?php
-function stanford_framework_form_system_theme_settings_alter(&$form, &$form_state) {
- 
-  // Header Text
-  $form['header_container'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Header'),
-    '#description' => t('Use these settings to adjust the header logo image and text display.'),
+function stanford_light_form_system_theme_settings_alter(&$form, &$form_state) {
+
+// Style Container
+  $form['style_container'] = array('#type' => 'fieldset',
+    '#title' => t('Style'),
+    '#description' => t('Use these settings to adjust the color palette and typography of your site.'),
     '#collapsible' => TRUE,
     '#collapsed' => FALSE,
   );
-  
-  $form['header_container']['logo_image_style_classes'] = array(
+
+// Color palettes
+   $form['style_container']['color_palette'] = array(
     '#type'          => 'radios',
-    '#title'         => t('Logo image style'),
-    '#default_value' => theme_get_setting('logo_image_style_classes'),
+    '#title'         => t('Color Palette'),
+    '#default_value' => theme_get_setting('color_palette'),
     '#options'       => array(
-      '' => t('Hide logo image in mobile - <strong><em>Default</em></strong>'),
-	  'logo-mobile ' => t('Show logo image in mobile'),
+	  'color-default' => t('Sandstone - <strong><em>Default</em></strong>'),
+      'color-bold' => t('Bold'),
+	  'color-monochrome' => t('Monochrome'),
     ),
   );
   
-  $form['header_container']['site_title_first_line'] = array(
-    '#type'          => 'textfield',
-    '#title'         => t('Site title (1st line) - <strong><em>Default is blank</em></strong>'),
-    '#default_value' => theme_get_setting('site_title_first_line'),
-    '#description'   => t('This will replace the Drupal default site title'),
-  );
-  
-  $form['header_container']['site_title_second_line'] = array(
-    '#type'          => 'textfield',
-    '#title'         => t('Site title (2nd line) - <strong><em>Default is blank</em></strong>'),
-    '#default_value' => theme_get_setting('site_title_second_line'),
-    '#description'   => t('This will replace the Drupal default site title'),
-  );
-    
-  $form['header_container']['site_title_style_classes'] = array(
+// Typography
+   $form['style_container']['typography'] = array(
     '#type'          => 'radios',
-    '#title'         => t('Site title style'),
-    '#default_value' => theme_get_setting('site_title_style_classes'),
+    '#title'         => t('Typography'),
+    '#default_value' => theme_get_setting('typography'),
     '#options'       => array(
-      '' => t('Display in title case - <strong><em>Default</em></strong>'),
-	  'site-title-uppercase ' => t('Display 1st line in bold and uppercase (reserved for schools)'),
+      'typography-default' => t('Professional - <strong><em>Default</em></strong>'),
+	  'typography-trendy' => t('Trendy'),
+	  'typography-writer' => t('Writer'),
     ),
   );
-  
-  $form['header_container']['site_title_position_classes'] = array(
-    '#type'          => 'radios',
-    '#title'         => t('Site title (2nd line) position'),
-    '#default_value' => theme_get_setting('site_title_position_classes'),
-    '#options'       => array(
-      '' => t('Display below site title - <strong><em>Default</em></strong>'),
-	  'site-title-underneath ' => t('Display below site logo image'),
-    ),
-  );
+
+// Red bar / no red bar
+
+// Header background image
+
+// Slogan
+
+// Logo
+
+// Background image
+
 }
