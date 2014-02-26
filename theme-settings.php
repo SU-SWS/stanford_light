48bb6db14e968940098e66729b9d50eda9d1d317
Brian Young <brianyoung@stanford.edu>
2014-02-26 10:56:33 -0800

    Updated theme options to match CSS file names

--- theme-settings.php
+++ theme-settings.php
@@ -1,35 +1,38 @@
 <?php
 function stanford_light_form_system_theme_settings_alter(&$form, &$form_state) {
 
-// Style Container
-  $form['style_container'] = array('#type' => 'fieldset',
-    '#title' => t('Style'),
-    '#description' => t('Use these settings to adjust the color palette and typography of your site.'),
+// Design Container
+  $form['design_container'] = array('#type' => 'fieldset',
+    '#title' => t('Customize design'),
+    '#description' => t('Use these settings to adjust the style and typography of your site.'),
     '#collapsible' => TRUE,
     '#collapsed' => FALSE,
   );
 
-// Color palettes
-   $form['style_container']['color_palette'] = array(
+// Styles
+   $form['design_container']['styles'] = array(
     '#type'          => 'radios',
-    '#title'         => t('Color Palette'),
-    '#default_value' => theme_get_setting('color_palette'),
+    '#title'         => t('Choose styles'),
+    '#default_value' => theme_get_setting('styles'),
     '#options'       => array(
-	  'color-default' => t('Sandstone - <strong><em>Default</em></strong>'),
-      'color-bold' => t('Bold'),
-	  'color-monochrome' => t('Monochrome'),
+	  'styles-light' => t('Light - <strong><em>Default</em></strong>'),
+      'styles-dark' => t('Dark'),
+	  'styles-bright' => t('Bright'),
+	  'styles-plain' => t('Plain'),
+	  'styles-rich' => t('Rich'),
+	  'styles-contrast' => t('High Contrast'),
     ),
   );
-  
-// Typography
-   $form['style_container']['typography'] = array(
+    
+// Fonts
+   $form['design_container']['fonts'] = array(
     '#type'          => 'radios',
-    '#title'         => t('Typography'),
-    '#default_value' => theme_get_setting('typography'),
+    '#title'         => t('Choose fonts'),
+    '#default_value' => theme_get_setting('fonts'),
     '#options'       => array(
-      'typography-default' => t('Professional - <strong><em>Default</em></strong>'),
-	  'typography-trendy' => t('Trendy'),
-	  'typography-writer' => t('Writer'),
+      'fonts-sans' => t('Sans Serif - <strong><em>Default</em></strong>'),
+	  'fonts-serif' => t('Serif'),
+	  'fonts-slab' => t('Slab'),
     ),
   );
 
