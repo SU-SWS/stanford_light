ce80f6491d9bd709fc798b355c14fcb17c6f8536
Brian Young <brianyoung@stanford.edu>
2014-02-28 10:15:56 -0800

    Enabled body background image option

--- theme-settings.php
+++ theme-settings.php
@@ -114,9 +114,6 @@ function stanford_light_form_system_theme_settings_alter(&$form, &$form_state) {
     ),
   );
 
-// Body background image Override
-// $form['background_container'] = array();
-
 // Border Style Override
 $form['border_container'] = array();
 
